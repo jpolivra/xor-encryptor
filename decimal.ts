6dc806f1b9bdcbd53052f8abf74608ebe4bb2526
Oliveira Joao <thxjao.dev@gmail.com>
2022-06-25 13:26:32 -0300

    style: formats some documentation files

--- decimal.ts
+++ decimal.ts
@@ -9,15 +9,15 @@
 
 // 1   0   1   1   0
 // |   |   |   |   |
-// 16   8   4   2   1
+// 16  8   4   2   1
 
 // Now we're going to add the numbers where
 // we have 1 (that means true).
 
 // 1   0   1   1   0
 // |   |   |   |   |
-// 16   8   4   2   1
-
-// 16 + 4 + 2 = 22
+// 16  8   4   2   1
+// |   0   |   |   0
+// 16  +   4 + 2 = 22
 
 // Now we've 22 that are the decimal of 10110
