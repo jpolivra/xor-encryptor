66d956221a92986fe0119f58bfb7615a033fce2f
Oliveira Joao <thxjao.dev@gmail.com>
2022-06-25 13:15:02 -0300

    style: fixes words misspellings

--- base64.ts
+++ base64.ts
@@ -8,14 +8,14 @@
 
 // Now we've to convert each eight bits binary to a six bits binary
 // That's because into Base64 can only be made into six bits binary
-// To make it is very easy, basicly we're gonna cut the whole
+// To make it is very easy, basically we're going to cut the whole
 // Binary sentence in to six digits, so it will be like it:
 // 010010 000110 010101 101100 011011 000110 1111
 
 // As you can se, now we have a 4 bits binary, so we can just add
-// At the end to finish the six bits convertion
+// At the end to finish the six bits conversion
 // 010010 000110 010101 101100 011011 000110 1111=
-// (Probably the = signal will be replaced by 00 into the covertion)
+// (Probably the = signal will be replaced by 00 into the conversion)
 
 // After this, the next step is to convert binary to decimal
 // (Check decimal.ts file to learn how to make it)
@@ -33,12 +33,12 @@
 
 // Collect the word (Hello)
 // ||
-//  ===> Tranform each character into binary
+//  ===> Transform each character into binary
 // ||
 //  ====> Probably from that you will take 8 digits binaries
 //        So you'll need to convert then into 6 digits binaries
 // ||
 //  =======> From that convert then into decimal
 // ||
-//   =========> From decimals get each char that correponds to it's value
+//   =========> From decimals get each char that corresponds to its value
 //              Present into the base64 encoding table
