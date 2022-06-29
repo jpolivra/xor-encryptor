2d4f6a204ef453643243d7807017df3bf097294e
Oliveira Joao <thxjao.dev@gmail.com>
2022-06-29 20:03:44 -0300

    feat: creates parser class

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// Number bases and Logical Mathematics
+
+export class ParseTo {
+    public logTestFunction(e: string) {
+        return e.toUpperCase()
+    }
+}
+
+const parser = new ParseTo()
+
+const testLog = parser.logTestFunction('teste')
+
+console.log(testLog)
